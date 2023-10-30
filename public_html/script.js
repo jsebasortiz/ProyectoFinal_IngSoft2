f70ea8710494ff5bea0864a67b1c1ab4e854ac89
Jhon Velez <jhon.velez01@uceva.edu.co>
2023-10-30 10:59:07 -0500

    Actualizar archivos a la tercera versión

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,7 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Este es un archivo JavaScript de la segunda versión.
-    console.log('¡Hola desde la segunda versión de JavaScript!');
+    // Este es un archivo JavaScript más avanzado para la tercera versión.
+    console.log('¡Bienvenido a la tercera versión de mi página web!');
+    
+    // Agregar interactividad aquí
 });

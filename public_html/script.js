30d4b304ab2ea34b0da1672dc709b1e07b69576d
Jhon Velez <jhon.velez01@uceva.edu.co>
2023-10-30 10:50:04 -0500

    Actualizar archivos a la segunda versión

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,5 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Este es un archivo JavaScript de ejemplo.
-    console.log('¡Hola desde JavaScript!');
+    // Este es un archivo JavaScript de la segunda versión.
+    console.log('¡Hola desde la segunda versión de JavaScript!');
 });
